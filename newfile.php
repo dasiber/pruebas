c4a4a33290d1fcedfe17acc5cfc727d099c811d8
Belen Ibanez Marcos <belen@Macintosh.local>
2014-02-11 18:11:37 +0000

    This chance takes something that has been removed in github

--- newfile.php
+++ newfile.php
@@ -1,2 +1,2 @@
 This would be a iGREAT    
-This is cool, this is the first time that I modified something in github, it can be very useful
+This is cool,yyyy this is the first time that I modified something in github, it can be  very very useful
